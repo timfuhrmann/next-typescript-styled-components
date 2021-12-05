ca34105297a0a407f4997ad366b95c85eebbe8b6
timfuhrmann <tf@becklyn.com>
2021-12-05 18:41:41 +0100

    Add ts paths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
-import { theme } from "../app/css/theme";
-import { GlobalStyle } from "../app/css/GlobalStyle";
+import { theme } from "@css/theme";
+import { GlobalStyle } from "@css/GlobalStyle";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
